4f8739ad17231c6e2f41bee21835040e63643dd3
boblandsky <flaminiron13-signups@yahoo.com>
2020-06-24 14:51:08 -0700

    setup for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run onion_or_not.py
--- onion_or_not.py
+++ onion_or_not.py
@@ -1,5 +1,8 @@
+import ast
+import random
 import re
 import string
+import pickle
 
 import nltk
 import pandas as pd
@@ -12,7 +15,7 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-import random
+from sklearn.naive_bayes import GaussianNB
 
 # ! used for preprocessing data
 # data = pd.read_csv('./data/onionornot.csv')
@@ -28,6 +31,15 @@ df = pd.read_csv(data_url)
 df.drop(columns=['Unnamed: 0'], inplace=True)
 df_headlines = df['text'].values
 df_labels = df['label'].values
+
+hl_train, hl_test, l_train, l_test = train_test_split(df_headlines, df_labels, random_state = 69, test_size = 0.3)
+
+vectorizer = TfidfVectorizer()
+
+vectorizer.fit(hl_train)
+
+X_train = vectorizer.transform(hl_train)
+X_test = vectorizer.transform(hl_test)
 
 onion = df[df['label'] == 1].copy()
 not_onion = df[df['label'] == 0].copy()
@@ -56,20 +68,30 @@ st.write(df.head())
 
 model_picker = st.sidebar.radio('Select different methods for prediction.',
                         ('Logistic Regression, Quick and Dirty',
+                         'Naive Bayes, Quick and Dirty',
                          'Naive Bayes, NLTK Processed')
                         )
 
+st.sidebar.markdown("The logistic regression model uses first vectorization using scikit-learn's "
+                    '[TFIDF](https://en.wikipedia.org/wiki/Tf%E2%80%93idf) Vectorizer  '
+                    'to adjust how important a word is to the corpus. '
+                    'The second and third models use a '
+                    '[naive Bayes classifer ](https://en.wikipedia.org/wiki/Naive_Bayes_classifier)'
+                    'for classification. The third model uses additional normalization '
+                    '(e.g. [lemmatization](https://en.wikipedia.org/wiki/Lemmatisation)). '
+                    )
+st.sidebar.info('"Quick and Dirty" means that I did not further process the data. '
+                'The full dataset from the Github is what is going into the models. '
+                'The NLTK processed data is split into test and train sets. '
+                'The train/test splits for all 3 methods are 70/30.'
+                )
+
+st.markdown('---')
+st.warning('Despite me saying "Quick and dirty", '
+           'running these models can take some time, please be patient.')
+
 if model_picker == 'Logistic Regression, Quick and Dirty':
     st.header('Model used: Logistic Regression')
-
-    hl_train, hl_test, l_train, l_test = train_test_split(df_headlines, df_labels, random_state = 69)
-
-    vectorizer = TfidfVectorizer()
-
-    vectorizer.fit(hl_train)
-
-    X_train = vectorizer.transform(hl_train)
-    X_test = vectorizer.transform(hl_test)
 
     clf_adj = LogisticRegression()
     clf_adj.fit(X_train, l_train)
@@ -79,7 +101,7 @@ if model_picker == 'Logistic Regression, Quick and Dirty':
     st.write(f'Accuracy on test set: {rounded_score}%')
 
     test_headline = st.text_input("Give me a headline to predict. A sample one is provided.",
-                                "CIA Realizes It's Been Using Black Highlighters All These Years")
+                                "MLS Commissioner Relieved That Nobody Knows Him by Name")
 
     if st.button('Onion or not?'):
         test_vect = vectorizer.transform([test_headline])
@@ -90,14 +112,14 @@ if model_picker == 'Logistic Regression, Quick and Dirty':
         else:
             st.write("It's from the Onion!")
 
-    lr_worked = st.radio('Did the logistic regression model make an accurate prediction?',
-                        ('Yes', 'No')
-                        )
+    # lr_worked = st.radio('Did the logistic regression model make an accurate prediction?',
+    #                     ('Yes', 'No')
+    #                     )
 
-    if lr_worked == 'No':
-        st.write('Bummer.')
-    else:
-        st.write('Sweet!')
+    # if lr_worked == 'No':
+    #     st.write('Bummer.')
+    # else:
+    #     st.write('Sweet!')
 
 elif model_picker == 'Naive Bayes, NLTK Processed':
     st.header('Model used: Naive Bayes, processed with NLTK')
@@ -125,7 +147,7 @@ elif model_picker == 'Naive Bayes, NLTK Processed':
                 cleaned_tokens.append(token.lower())
         return cleaned_tokens
 
-
+    # ! only use for initial preprocessing of data
     # headline_tokens_cleaned = [remove_noise(x, stop_words) for x in headline_tokens]
     # onion_tokens = [word_tokenize(x) for x in onion_list]
     # not_onion_tokens = [word_tokenize(x) for x in not_onion_list]
@@ -154,24 +176,59 @@ elif model_picker == 'Naive Bayes, NLTK Processed':
     # pd.DataFrame(train_data).to_csv('train_data.csv', index=False)
     # pd.DataFrame(test_data).to_csv('test_data.csv', index=False)
 
-    # train_url = 'https://github.com/boblandsky/onion_ml/raw/master/train_data.csv'
-    # test_url = 'https://github.com/boblandsky/onion_ml/raw/master/test_data.csv'
+    train_url = 'https://github.com/boblandsky/onion_ml/raw/master/train_data.csv'
+    test_url = 'https://github.com/boblandsky/onion_ml/raw/master/test_data.csv'
 
-    # train_data = pd.read_csv(train_url, usecols=[1, 2])
-    # test_data = pd.read_csv(test_url, usecols=[1, 2])
-    # st.write(train_data)
+    train_data = pd.read_csv(train_url)
+    test_data = pd.read_csv(test_url)
 
-    # clf_nb = NaiveBayesClassifier.train(train_data)
-    # nb_acc = round(classify.accuracy(clf_nb, test_data), 4)*100
-    # st.write(f'Accuracy is {nb_acc}%')
+    train_data['0'] = train_data['0'].apply(lambda x: ast.literal_eval(x))
+    test_data['0'] = test_data['0'].apply(lambda x: ast.literal_eval(x))
 
-    # test_nb_headline = st.text_input("Give me a headline to predict. A sample one is provided.",
-    #                                   "MLS Commissioner Relieved That Nobody Knows Him by Name")
+    train_data_tuple = list(zip(train_data['0'], train_data['1']))
+    test_data_tuple = list(zip(test_data['0'], test_data['1']))
+    # st.write(type(train_data['0'][0]))
 
-    # if st.button('Onion or not? Round 2'):
-    #     test_nb_tokens = remove_noise(word_tokenize(test_nb_headline))
-    #     results_nb = clf_nb.classify(dict([token, True] for token in test_nb_tokens))
-    #     if results_nb == 1:
-    #         st.write("It's from the Onion!")
-    #     else:
-    #         st.write("It's not from the Onion!")
+    clf_nb = NaiveBayesClassifier.train(train_data_tuple)
+    nb_acc = round(classify.accuracy(clf_nb, test_data_tuple), 4)*100
+    st.write(f'Accuracy on test set: {nb_acc}%')
+
+    test_nb_headline = st.text_input("Give me a headline to predict. A sample one is provided.",
+                                      "MLS Commissioner Relieved That Nobody Knows Him by Name")
+
+    if st.button('Onion or not? Round 3'):
+        test_nb_tokens = remove_noise(word_tokenize(test_nb_headline))
+        results_nb = clf_nb.classify(dict([token, True] for token in test_nb_tokens))
+        if results_nb == 1:
+            st.write("It's from the Onion!")
+        else:
+            st.write("It's not from the Onion!")
+
+    # nb_worked = st.radio('Did the Naive Bayes model make an accurate prediction?',
+    #                     ('Yes', 'No')
+    #                     )
+
+    # if nb_worked == 'No':
+    #     st.write('Bummer.')
+    # else:
+    #     st.write('Sweet!')
+
+if model_picker == 'Naive Bayes, Quick and Dirty':
+    clf_gnb = GaussianNB()
+    clf_gnb.fit(X_train.toarray(), l_train)
+    score = clf_gnb.score(X_test.toarray(), l_test)
+    rounded_score = round(score, 4)*100
+    # print('Accuracy: ', score)
+    st.write(f'Accuracy on test set: {rounded_score}%')
+
+    test_gnb_headline = st.text_input("Give me a headline to predict. A sample one is provided.",
+                                      "MLS Commissioner Relieved That Nobody Knows Him by Name")
+
+    if st.button('Onion or not? Round 2'):
+        test_vect = vectorizer.transform([test_gnb_headline])
+        results = clf_gnb.predict(test_vect.toarray())
+        #st.write(results)
+        if results[0] == 0:
+            st.write("It's not from the Onion!")
+        else:
+            st.write("It's from the Onion!")
